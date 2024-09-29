c531ab6078a1e971f8969acc5c5a72a7df6bbf23
Randi Himawan <randi.mybook@gmail.com>
2024-09-29 20:34:34 +0700

    Re-Organize Folder, Create Simple SERVER, etc

---
+++ 0. ROUTER/router.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET",
+  })
+);
+
+// Middleware untuk melayani file static di public (untuk kebutuhan aplikasi sendiri)
+app.use(express.static(path.join("public")));
+
+// Middleware CORS, hanya berlaku untuk rute coba.json
+app.get("/DATA/coba.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "DATA", "coba.json"));
+});
+
+// Middleware untuk memeriksa domain lain hanya bisa mengakses coba.json
+app.use((req, res, next) => {
+  const origin = req.get("origin");
+  if (origin && req.path !== "/DATA/coba.json") {
+    return res.status(403).send("Access Forbidden");
+  }
+  // Middleware untuk melayani file static di public (untuk kebutuhan aplikasi sendiri)
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>First Page is Running</h1>");
+});
+
+// Apabila URL halaman tidak di temukan
+app.use("/", (req, res) => {
+  res.status(404).send("Page is not Found !");
+});
+
+const port = 3000;
+const host = "127.0.0.1";
+app.listen(port, host, () => {
+  console.log(`Server Running on ${host}:${port}`);
+});
---
+++ 1. LATIHAN-1/app.js
@@ -0,0 +1,13 @@
+async function runFectch() {
+  try {
+    const response = await fetch("http://127.0.0.1:3000/DATA/coba.json");
+    if (!response.ok) {
+      throw new Error("Nertwork Response is Protblem" + response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+runFectch();
