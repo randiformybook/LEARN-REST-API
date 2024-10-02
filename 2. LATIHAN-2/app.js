15f1d4b9d424305fc84d643d1976a84e89f7530b
randiformybook <randi.mybook@gmail.com>
2024-10-02 14:19:22 +0700

    "Updated menu.json with new menu items and descriptions, modified app.js to handle category filtering and updated index.html with new dropdown menu options"

--- 2. LATIHAN-2/app.js
+++ 2. LATIHAN-2/app.js
@@ -1,11 +1,27 @@
 async function runFectch() {
   try {
     const response = await fetch("http://127.0.0.1:3000/DATA/menu.json");
+
+    // Jika fetch gagal
     if (!response.ok) {
       throw new Error("Nertwork Response is Protblem" + response.statusText);
     }
     const data = await response.json();
+    // Tampilkan semua menu saat pertama kali halaman dimuat
     await updateMenu(data.menu);
+
+    // Event listener untuk dropdown kategori
+    const categoryMenu = document.getElementById("menu-category");
+    categoryMenu.addEventListener("click", async (e) => {
+      const category = e.target.dataset.category;
+      if (category) {
+        // Ganti teks button sesuai kategori yang dipilih
+        const dropDownBtn = document.querySelector(".btn-group .btn");
+        dropDownBtn.textContent = e.target.textContent;
+        // Jalan Function Filter Menu berdasarkan Category
+        await filterMenuByCategory(data.menu, category);
+      }
+    });
   } catch (err) {
     console.log(err);
   }
@@ -22,7 +38,7 @@ function updateMenu(allMenu) {
 
 function menuData(menu) {
   const origin = "http://127.0.0.1:3000";
-  return `<div class="col-md-4">
+  return `<div class="col-md-3 mt-3">
             <div class="card">
               <img src="${origin}${menu.gambar}" class="card-img-top" />
                 <div class="card-body">
@@ -34,6 +50,18 @@ function menuData(menu) {
                 </div>
               </div>
             </div>`;
+}
+
+async function filterMenuByCategory(allMenu, category) {
+  let filterMenu;
+  if (category === "all") {
+    filterMenu = allMenu;
+  } else {
+    filterMenu = await allMenu.filter(
+      (menu) => menu.kategori.toLowerCase() === category.toLowerCase()
+    );
+  }
+  await updateMenu(filterMenu);
 }
 
 runFectch();
